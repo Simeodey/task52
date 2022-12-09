8a5bed7cc9fe85a9fcf25405c93d5a1426790258
Simeodey <adamand12@abv.bg>
2022-12-09 18:54:09 +0200

    asd

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
+  document.body.addEventListener("click", () => {
+    const arr  = Array(5).fill(0).map(el=> {
+      const article = document.createElement('article')
+    article.className = 'message'
+    article.innerHTML = `The world will end in ${Math.random()*100} seconds`
+  return article})
+  
+  document.body.append(...arr)
   });
 });
